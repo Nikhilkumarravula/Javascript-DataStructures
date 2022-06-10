401950add9a3a6f9a7850afc2723c7f963a8314f
Nikhilkumarravula <103506842+Nikhilkumarravula@users.noreply.github.com>
2022-06-10 10:01:45 +0530

    Create BinarySearchTree.js

---
+++ BinarySearchTree.js
@@ -0,0 +1,38 @@
+class Node{
+    constructor(data){
+        this.data=data
+        this.left=null
+        this.right=null
+    }
+}
+class bst{
+    constructor(){
+        this.root=null
+    }
+    insert(data){
+        newnode= new Node(data)
+        if(!this.root){
+            this.root=newnode
+        }else{
+            insertnode(this.root , newnode)
+        }
+    }
+    insertnode(root , node){
+        if(node.data<root.data){
+            if(!root.left){
+                root.left=node
+            }else{
+                insertnode(root.left , node)
+            }
+        }else if(node.data>root.data){
+            if(!root.right){
+                root.right=node
+            }else{
+                insertnode(root.right , node)
+            }
+        }else{
+            console.log("data already exists")
+        }
+    }
+    
+}
