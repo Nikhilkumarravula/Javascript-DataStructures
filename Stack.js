06ed635a6f11d5f1288ee2f2343f8db190284d66
Nikhilkumarravula <103506842+Nikhilkumarravula@users.noreply.github.com>
2022-06-26 12:31:17 +0530

    Update Stack.js

--- Stack.js
+++ Stack.js
@@ -1,1 +1,32 @@
-stack
+arr=[]
+count=0
+stack=function(){
+    this.push=function(data){
+        arr.push(data)
+        count++
+    }
+    this.pop=function(){
+        arr.pop()
+        count--
+    }
+    this.peek = function() {
+        return console.log(arr[count-1])
+    }
+    this.size=function(){
+        return console.log(count)
+    }
+}
+mystack=new stack()
+mystack.push(12)
+mystack.push(4)
+mystack.push(9)
+mystack.size()
+mystack.push(120)
+mystack.pop()
+mystack.size()
+mystack.push(145)
+mystack.pop()
+mystack.pop()
+mystack.push("free")
+mystack.size()
+mystack.peek()
