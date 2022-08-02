0f32cda8902b56122120d885b0eee9e7c3c5b4e7
Nikhilkumarravula <103506842+Nikhilkumarravula@users.noreply.github.com>
2022-08-02 17:44:59 +0530

    Update Heap.js

--- Heap.js
+++ Heap.js
@@ -73,11 +73,14 @@ console.log(h.data)
 
 
 /*Heapify algorithm for array*/
+
 function heapify(arr,n,i){
-    let largest=i
-    let left=2*i
-    let right=(2*i)+1
-    if(left<n && arr[largest]<arr[left]){
+    while(i>=0){
+        
+        let largest=i
+    let left=(2*i)+1
+    let right=(2*i)+2
+        if(left<n && arr[largest]<arr[left]){
         largest=left
     }
     if(right<n && arr[largest]<arr[right]){
@@ -86,12 +89,15 @@ function heapify(arr,n,i){
     if(largest!=i){
         [arr[largest],arr[i]]=[arr[i],arr[largest]]
         heapify(arr,n,largest)
+    }else{
+        return
     }
-    return arr
+    }
+    
 }
-let arr=[-1,54,53,55,52,50]
-let n=arr.length-1
-for(let i=Math.floor(n/2);i>0;i--){
-    heapify(arr,n,i)
+nums=[3,4,5,2]
+let n=nums.length
+for(let i=Math.floor(n/2);i>=0;i--){
+    heapify(nums,n,i)
 }
 console.log(arr)
